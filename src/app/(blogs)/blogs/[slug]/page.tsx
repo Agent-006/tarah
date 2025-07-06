ed87a4d452eaaac88ee831b27ddcaf993aa4d88b
Sagar~ <sagargope2001@gmail.com>
2025-07-06 17:52:05 +0530

    refactor: simplify BlogPostPage component by removing Suspense and Loading

--- src/app/(blogs)/blogs/[slug]/page.tsx
+++ src/app/(blogs)/blogs/[slug]/page.tsx
@@ -1,19 +1,9 @@
 import BlogPost from "@/components/blog/BlogPost";
-import React, { Suspense } from "react";
-import Loading from "./loading";
 
-const BlogPostPage = ({
-    params,
-}: {
-    params: { slug: string };
-}) => {
+export default function BlogPostPage({ params }: { params: { slug: string } }) {
     return (
         <section className="bg-secondary flex flex-col items-center justify-center min-h-screen p-4">
-            <Suspense fallback={<Loading />}>
-                <BlogPost params={params}/>
-            </Suspense>
+            <BlogPost params={params} />
         </section>
     );
-};
-
-export default BlogPostPage;
+}
